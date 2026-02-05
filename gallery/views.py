64fe6f7021a816932d72e7947696cfa130aacc2a
denis-kolbasenko <denisvip2004@mail.ru>
2026-02-05 20:13:44 +0700

    qwewe

--- gallery/views.py
+++ gallery/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # request — это "письмо" от браузера с данными о пользователе
 def home(request):
 # Мы пока не используем HTML-шаблоны, просто вернем строку.
-    return HttpResponse("<h1>Добро пожаловать в 3D Хранилище</h1><p>Система работает.</p>")
+    return HttpResponse("<h1>Добро пожаловать в 3D [[[[[[Хранилище</h1><p>Система работает.</p>")
 def about(request):
 # Мы пока не используем HTML-шаблоны, просто вернем строку.
     return HttpResponse("<h1> Курс Web Структуры </h1><p>Система работает.</p>")
