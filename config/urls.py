14d886887e1896ea7552586ee557663e6522c7ea
denis-kolbasenko <denisvip2004@mail.ru>
2026-02-07 12:06:17 +0700

    Обновил, urls и templates

--- config/urls.py
+++ config/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.urls import path
 # Импортируем нашу функцию из приложения gallery
 from gallery.views import home
-from gallery.views import about
+#from gallery.views import about
 urlpatterns = [
 path('admin/', admin.site.urls),
 
 # Пустая строка '' означает главную страницу сайта (http://localhost:8000/)
 path('', home, name='home'),
-path('about/', about, name='about'),
+#path('about/', about, name='about'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# request — это "письмо" от браузера с данными о пользователе
 def home(request):
-# Мы пока не используем HTML-шаблоны, просто вернем строку.
-    return HttpResponse("<h1>Добро пожаловать в 3D [[[[[[Хранилище</h1><p>Система работает.</p>")
-def about(request):
-# Мы пока не используем HTML-шаблоны, просто вернем строку.
-    return HttpResponse("<h1> Курс Web Структуры </h1><p>Система работает.</p>")
+    # Имитация данных из базы (список словарей)
+    fake_database = [
+        {'id': 1, 'name': 'Sci-Fi Helmet', 'file_size': '15 MB'},
+        {'id': 2, 'name': 'Old Chair', 'file_size': '2 MB'},
+        {'id': 3, 'name': 'Cyber Truck', 'file_size': '10 MB'},
+        {'id': 4, 'name': 'Cyber000 Truck', 'file_size': '20 MB'},
+    ]
+    context_data = {
+        'page_title': 'Главная Галерея',
+        'assets': fake_database, # Передаем весь список
+    }
+    return render(request, 'gallery/index.html', context_data)
